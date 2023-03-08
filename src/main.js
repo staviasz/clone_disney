854d4ff449009df78f19c5fc331e7b84098b93f6
Erick Staviasz <erickesc21@hotmail.com>
2023-03-08 17:22:11 -0300

    ajustes mobile

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-fac-question]');
     
+    const hero_section = document.querySelector('.hero');
+    const altura_hero = hero_section.clientHeight;
 
+    window.addEventListener('scroll', function() {
+        const posicao_atual = window.scrollY;
+
+        if (posicao_atual < altura_hero) {
+            oculta_elementos_header();
+        } else {
+            exibe_elementos_header();
+        }
+    });
+    
+    //secão de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const aba_alvo = botao.target.dataset.tabButton;
@@ -13,7 +27,30 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
+    // seção FAQ, accordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abre_ou_fecha_resposta);
+    }
+
 });
+
+function oculta_elementos_header() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibe_elementos_header() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function abre_ou_fecha_resposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    console.log(elemento);
+    const elemento_pai = elemento.target.parentNode;
+
+    elemento_pai.classList.toggle(classe)
+}
 
 function remove_botao_ativo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
